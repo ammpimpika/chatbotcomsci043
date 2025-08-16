1b39d9e77c5d9289405a8df439e60b592b7cced7
ammpimpika <650112230043@bru.ac.th>
2025-08-16 15:12:56 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,19 +1,1 @@
-import mysql from "mysql2/promise";
 
-export default async function handler(req, res) {
-  try {
-    const connection = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-    });
-
-    const [rows] = await connection.execute("SELECT * FROM alumni LIMIT 10");
-    await connection.end();
-
-    res.status(200).json(rows);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-}
